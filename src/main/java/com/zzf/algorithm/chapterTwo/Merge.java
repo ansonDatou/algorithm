394f01bbffe8fec19440e9887350b5445292e523
张植富 <jeffery@zhangyimais-MacBook-Pro.local>
2019-12-24 13:48:39 +0800

    add merge sort

--- src/main/java/com/zzf/algorithm/chapterTwo/Merge.java
+++ src/main/java/com/zzf/algorithm/chapterTwo/Merge.java
@@ -10,11 +10,22 @@ public class Merge {
 
     private static int mergeIndex = 0;
 
+    /**
+     * 归并
+     * 将元素复制到aux临时数组中
+     * 比较：左边是否小于mid，右边是否小于hi，比较右值是否小于左值，else否则正常左值小于右值
+     * @param a
+     * @param lo
+     * @param mid
+     * @param hi
+     * @param direction
+     */
     public static void merge(Comparable[] a, int lo, int mid, int hi, String direction) {
         mergeIndex++;
         System.out.println("【merge "+ direction +"】= lo : " + lo + ", mid : " + mid + ", hi : " + hi + ", mergeIndex : " + mergeIndex);
 
         // i = 低，j = min+1，k = 索引
+        // 将元素复制到aux中
         int i = lo, j = mid + 1;
         for (int k = lo; k <= hi; k++) {
             aux[k] = a[k];
@@ -27,24 +38,28 @@ public class Merge {
             System.out.println("k = " + k + ", i = " + i + ", j = " + j);
             if (i > mid) {
                 // i > mid : i < mid 说明左边比较完了
+                // 左半边用尽（取右半边元素）
                 System.out.println("(i > mid) : " + i +" > "+ mid + ", j+1 = "+ (j+1));
                 a[k] = aux[j++];
                 System.out.println("a[k] : a["+k+"] = " + a[k]);
                 System.out.println();
             } else if (j > hi) {
                 // j > hi : j=min+1 > hi 说明右边比较完了
+                // 右半边用尽（取左半边元素）
                 System.out.println("(j > hi) : " + j +" > "+ hi + ", i+1 = " + (i+1));
                 a[k] = aux[i++];
                 System.out.println("a[k] : a["+k+"] = " + a[k]);
                 System.out.println();
             } else if (Example.less(aux[j], aux[i])) {
                 // aux[j] < aux[i] : 右边值 < 左边值 则替换
+                // 右半边的当前元素 <, 左半边的当前元素（取右半边元素）
                 System.out.println("(Example.less) aux["+j+"] = " + aux[j] + ", aux["+i+"] = " + aux[i]);
                 a[k] = aux[j++];
                 System.out.println("a[k] : a["+k+"] = " + a[k]);
                 System.out.println();
             } else {
                 // 右边值 > 左边值 则不替换
+                // 右半边的当前元素 >= 左半边的当前元素（取左半边元素）
                 System.out.println("(else) aux[i] : aux["+i+"] = " + aux[i]);
                 a[k] = aux[i++];
                 System.out.println("(else) " + "a[k] : a["+k+"] = " + a[k]);
@@ -64,6 +79,13 @@ public class Merge {
         sort(a, 0, a.length - 1, "all");
     }
 
+    /**
+     * 分割，分割为左边和右边
+     * @param a
+     * @param lo
+     * @param hi
+     * @param direction
+     */
     public static void sort(Comparable[] a, int lo, int hi, String direction) {
         sortIndex++;
 
--- src/main/java/com/zzf/algorithm/testcase/Main.java
+++ src/main/java/com/zzf/algorithm/testcase/Main.java
@@ -5,11 +5,49 @@ import com.zzf.algorithm.chapterTwo.Example;
 
 public class Main {
 
+    public static Comparable[] aux;
+
     public static void main(String[] args) {
         // 9 8 7 6 5 4 3 2 1 0
         String[] a = In.readStrings();
-        shell(a);
+        sort(a);
         Example.show(a, "a");
+    }
+
+    public static void merge(Comparable[] a, int lo, int mid, int hi) {
+
+        int i = lo, j = mid+1;
+        for (int k = lo; k <= hi; k++) {
+            aux[k] = a[k];
+        }
+
+        for (int k = lo; k <= hi; k++) {
+            if (i > mid) {
+                a[k] = aux[j++];
+            } else if (j > hi) {
+                a[k] = aux[i++];
+            } else if (Example.less(aux[j], aux[i])) {
+                a[k] = aux[j++];
+            } else {
+                a[k] = aux[i++];
+            }
+        }
+    }
+
+    public static void sort(Comparable[] a) {
+        aux = new Comparable[a.length];
+        sort(a, 0, a.length - 1);
+    }
+
+    public static void sort(Comparable[] a, int lo, int hi) {
+        if (hi <= lo) {
+            return;
+        }
+
+        int mid = lo + (hi - lo) / 2;
+        sort(a, lo, mid);
+        sort(a, mid+1, hi);
+        merge(a, lo, mid, hi);
     }
 
     public static void select(Comparable[] a) {
