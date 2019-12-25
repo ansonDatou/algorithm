99dce655bf2fed77bed0b3d26d3c0ef5699080b8
张植富 <jeffery@zhangyimais-MacBook-Pro.local>
2019-12-25 14:22:33 +0800

    add quick sort

--- src/main/java/com/zzf/algorithm/chapterTwo/Quick.java
+++ src/main/java/com/zzf/algorithm/chapterTwo/Quick.java
@@ -14,6 +14,7 @@ public class Quick {
     }
 
     private static void sort(Comparable[] a, int lo, int hi, String direction) {
+        Example.show(a, "【sort top】a");
 
         // 递归退出条件
         if (hi <= lo) {
@@ -21,7 +22,10 @@ public class Quick {
         }
 
         int j = partition(a, lo, hi);
+
         System.out.println("【sort "+ direction +"】" + "j = " + j + ", lo = " + lo + ", hi = " + hi);
+        Example.show(a, "【sort "+ direction +"】a");
+
         sort(a, lo, j - 1, "left");
         sort(a, j + 1, hi, "right");
     }
@@ -52,7 +56,8 @@ public class Quick {
             // 因为从i是基准，所以从++i开始比较
             // false 退出，说明 a[++i] > v，则需要把a[++i]放到v的右边（左指针扫描到一个值，应该在v的右边）
             while (Example.less(a[++i], v)) {
-                // 左指针==高则退出
+                // 左指针==高，则退出，说明左指针已经从低到高扫描完了
+                System.out.println("i:"+ i +" == hi:"+ hi +" " + (i == hi));
                 if (i == hi) {
                     break;
                 }
@@ -63,7 +68,8 @@ public class Quick {
             // 扫描右，检查扫描是否结束并交换元素
             // false 退出，说明 v > a[--j]，则需要把a[--j]放到v的左边（右指针扫描到一个值，应该在v的左边）
             while (Example.less(v, a[--j])) {
-                // 右指针==低则退出
+                // 右指针==低，则退出，说明右指针已经从高到底扫描完了
+                System.out.println("j:"+ j +" == lo:"+ lo +" " + (j == lo));
                 if (j == lo) {
                     break;
                 }
@@ -74,6 +80,8 @@ public class Quick {
                 break;
             }
 
+            // i需要交换，基于上面的：false 退出，说明 a[++i] > v
+            // j需要交换，基于上面的：false 退出，说明 v > a[--j]
             Example.exch(a, i, j);
 
             Example.show(a, "while 内 a");
@@ -81,8 +89,12 @@ public class Quick {
             System.out.println();
         }
 
+        // a[lo] == v == 基准，基准与中间位置交换，因为基准应该在中间，所以交换
+        // a[lo]是基准元素，j是基准坐标，所以lo和j进行交换
         Example.exch(a, lo, j);
 
+        System.out.println();
+        System.out.println("【partition】lo = " + lo + ", j = " + j);
         Example.show(a, "while 外 a");
         return j;
     }
@@ -90,6 +102,7 @@ public class Quick {
     public static void main(String[] args) {
         // 9 8 7 6 5 4 3 2 1 0
         // 9 1 7 3 5 4 6 2 8 0
+        // 5 2 3 4 1
         String[] a = In.readStrings();
         sort(a);
         assert Example.isSorted(a);
