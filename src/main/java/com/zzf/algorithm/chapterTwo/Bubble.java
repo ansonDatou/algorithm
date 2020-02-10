ec6ada366851ccf4c8f369afa7a0da8bba9ff2cf
张植富 <jeffery@zhangyimais-MacBook-Pro.local>
2020-02-10 10:18:32 +0800

    id

--- src/main/java/com/zzf/algorithm/chapterTwo/Bubble.java
+++ src/main/java/com/zzf/algorithm/chapterTwo/Bubble.java
@@ -12,14 +12,14 @@ public class Bubble {
         // s o r t e x a m p l e
         // 9 1 7 3 5 4 6 2 8 0
         String[] a = In.readStrings();
-        sort(a);
+        sort1(a);
         assert Example.isSorted(a);
         Example.show(a, "a");
     }
 
     public static void sort(Comparable[] a) {
         for (int i = 1; i < a.length; i++) {
-            boolean flag = false;
+            boolean flag = true;
             for (int j = 0; j < a.length - i; j++) {
                 if (Example.less(a[j+1], a[j])) {
                     Example.exch(a,j+1, j);
@@ -28,6 +28,16 @@ public class Bubble {
             }
             if (flag) {
                 break;
+            }
+        }
+    }
+
+    public static void sort1(Comparable[] a) {
+        for (int x = 0; x < a.length - 1; x++) {
+            for (int y = 0; y < a.length - 1 - x; y++) {
+                if (Example.less(a[y+1], a[y])) {
+                    Example.exch(a, y+1, y);
+                }
             }
         }
     }
